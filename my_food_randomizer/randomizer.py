46ddc1fb888700da447ec3c30cd1e469c91ed66d
nikitatata <snimel@se.zgrad.ru>
2026-06-10 11:22:52 +0300

    улучшил рандомайзер, удалил комментарии

--- my_food_randomizer/randomizer.py
+++ my_food_randomizer/randomizer.py
@@ -7,29 +7,6 @@ class WeekGenerator:
     def __init__(self, dishes: List[Dish]):
         self.dishes = dishes # Вся еда
 
-    # def _get_candidates_food_type(self, food_type: FoodType):
-    #     suitable_foods = []
-    #     for dish in self.dishes:
-    #         if dish.food_type == food_type:
-    #             suitable_foods.append(dish)
-    #     return suitable_foods
-# Подбирает еду по типу
-
-    # def _get_candidates_food_device(self, food_devices: list[FoodDevice]):
-    #     suitable_foods = []
-    #     for dish in self.dishes:
-    #         if dish.food_device in food_devices:
-    #             suitable_foods.append(dish)
-    #     return suitable_foods
-# Подбирает еду по способу приготовления
-
-    # def _get_candidates_preferences(self, preference: Preference):
-    #     suitable_foods = []
-    #     for dish_food in self.dishes:
-    #         if preference in dish_food.preferences:
-    #             suitable_foods.append(dish_food)
-    #     return suitable_foods
-
     def _get_candidates(self, preference: Preference, food_device: list[FoodDevice]):
         suitable_foods1 = []
         suitable_foods2 = []
@@ -41,56 +18,50 @@ class WeekGenerator:
                 suitable_foods2.append(dish_food)
         return suitable_foods2
 
-    def recursive_randomize(self, count_of_dishes: int, list_food: list[Dish], shedule: list[Dish]):
+    def recursive_randomize(self, count_of_dishes: int, list_food: list[Dish], forbidden_food: list[Dish], shedule: list[Dish]):
         if len(shedule) >= count_of_dishes:
             return []
         if len(shedule) < count_of_dishes:
             condidate = random.choice(list_food)
-            while condidate in shedule:
-                condidate = random.choice(list_food)
+            if (shedule != [] and condidate in shedule) or (forbidden_food != [] and condidate in forbidden_food):
+                while condidate in shedule:
+                    condidate = random.choice(list_food)
             mas = []
             for num in range(condidate.eating_time):
                 mas.append(condidate)
-            if len(shedule) + len(mas) <= count_of_dishes:
-                return self.recursive_randomize(count_of_dishes, list_food, shedule + mas) + mas
+            length = len(mas) + len(shedule)
+            if length <= count_of_dishes or forbidden_food == []:
+                return self.recursive_randomize(count_of_dishes, list_food, forbidden_food, shedule + mas) + [condidate]
             else:
-                return self.recursive_randomize(count_of_dishes, list_food, shedule)
-        # return self.recursive_randomize(count_of_dishes, list_food, shedule)
+                return self.recursive_randomize(count_of_dishes, list_food, forbidden_food, shedule)
 
     def randomize(self, count_of_dishes: int, food_devices: list[FoodDevice], preference: Preference):
         foods = self._get_candidates(preference, food_devices)
 
         breakfasts = []
-        dinners_first = []
-        dinners_second = []
+        garnish = []
+        meat = []
         fruits = []
         others = []
         for food in foods:
             if food.food_type == "завтрак":
                 breakfasts.append(food)
             elif food.food_type == "гарнир" or food.food_type == "жидкое":
-                dinners_first.append(food)
+                garnish.append(food)
             elif food.food_type == "рыба" or food.food_type == "мясо":
-                dinners_second.append(food)
+                meat.append(food)
             elif food.food_type == "фрукт":
                 fruits.append(food)
             else:
                 others.append(food)
 
-        list_food = self.recursive_randomize(count_of_dishes, breakfasts, [])
+        list_food_breakfasts = self.recursive_randomize(count_of_dishes, breakfasts, [], [])
+        list_food_garnish = self.recursive_randomize(count_of_dishes, garnish, [], [])
+        list_food_meat = []
+        for elem in list_food_garnish:
+            list_food_meat += self.recursive_randomize(elem.eating_time, meat, list_food_meat, [])
+        list_food_others = self.recursive_randomize(count_of_dishes // 2, others, [], []) + [random.choice(fruits)]
 
-        # condidate = random.choice(breakfasts)
-        # while condidate in list_food:
-        #     condidate = random.choice(list_food)
-        #     mas = []
-        #     for num in range(condidate.num_of_dishes):
-        #         mas.append(condidate)
-
-        # for i in range(count_of_dishes):
-        #     temp_dish = random.choice(foods)
-        #     if temp_dish not in list_food:
-        #         list_food.append(temp_dish)
-
-        return list_food
+        return (list_food_breakfasts, list_food_garnish, list_food_meat, list_food_others)
 # Создаёт расписание
 # Подходящие блюда задаются либо способом приготовления, либо типом еды
