bb6cf9f2f7fb0e44e4ddf9d416c749e89568b01e
nikitatata <snimel@se.zgrad.ru>
2026-06-17 13:31:29 +0300

    улучшил рандомайзер, привёл код в относительный порядок

--- my_food_randomizer/main.py
+++ my_food_randomizer/main.py
@@ -1,18 +1,14 @@
-from my_food_randomizer.dish_classes import FoodType, FoodDevice, Preference
+from my_food_randomizer.dish_classes import FoodDevice, Preference
 from my_food_randomizer.load_dishes import load_dish
 from my_food_randomizer.randomizer import WeekGenerator
 
 def main():
     dishes = load_dish()
     wg = WeekGenerator(dishes)
-    list_of_fruits = wg.randomize(7, [FoodDevice("духовка"), FoodDevice("плита"), FoodDevice("аэрогриль"), FoodDevice("холодильник"), FoodDevice("микроволновка"), FoodDevice("мультиварка")], Preference("лето"))
-    for elem in list_of_fruits:
-        print(*elem, sep="\n")
+    list_of_food = wg.get_shedule(7, [FoodDevice("духовка"), FoodDevice("плита"), FoodDevice("аэрогриль"), FoodDevice("холодильник"), FoodDevice("микроволновка"), FoodDevice("мультиварка")], Preference("лето"))
+    for food in list_of_food:
+        print(*food, sep="\n")
         print("----")
-
-    # for i in dishes:
-    #     if i.food_type in ["мясо", "рыба"]:
-    #         print(i)
 
 if __name__ == '__main__':
     main()
--- my_food_randomizer/randomizer.py
+++ my_food_randomizer/randomizer.py
@@ -1,62 +1,77 @@
 import random
 from typing import List
-from my_food_randomizer.dish_classes import Dish, FoodType, FoodDevice, Preference
+from datetime import timedelta
+from my_food_randomizer.dish_classes import Dish, FoodDevice, Preference
 
 
 class WeekGenerator:
     def __init__(self, dishes: List[Dish]):
         self.dishes = dishes # Вся еда
 
-    def _get_candidates(self, preference: Preference, food_device: list[FoodDevice]):
-        suitable_foods1 = []
-        suitable_foods2 = []
+
+    def _get_candidates(self, preference: Preference, food_device: list[FoodDevice]) -> list[Dish]:
+        """
+        :param preference: предпочтения в еде
+        :param food_device: устройство, на котором готовят блюдо
+        :return: список блюд, подходящих по предпочтениям и девайсу
+        """
+        suitable_foods = []
         for dish_food in self.dishes:
-            if dish_food.food_device in food_device or dish_food.food_device == "-":
-                suitable_foods1.append(dish_food)
-        for dish_food in suitable_foods1:
-            if preference in dish_food.preferences:
-                suitable_foods2.append(dish_food)
-        return suitable_foods2
+            if (dish_food.food_device in food_device or dish_food.food_device == "-") and preference in dish_food.preferences:
+                suitable_foods.append(dish_food)
+        return suitable_foods
 
-    # def recursive_randomize(self, count_of_dishes: int, list_food: list[Dish], forbidden_food: list[Dish], shedule: list[Dish], shedule_length: int, meat_flag: bool):
-    #     if shedule_length >= count_of_dishes:
-    #         return []
-    #     if shedule_length < count_of_dishes:
-    #         condidate = random.choice(list_food)
-    #         if condidate in shedule or condidate in forbidden_food:
-    #             while condidate in shedule or condidate in forbidden_food:
-    #                 condidate = random.choice(list_food)
-    #         length = shedule_length + condidate.eating_time
-    #         if length <= count_of_dishes or (meat_flag is False):
-    #             return self.recursive_randomize(count_of_dishes, list_food, forbidden_food, shedule + [condidate], length, meat_flag) + [condidate]
-    #         else:
-    #             return self.recursive_randomize(count_of_dishes, list_food, forbidden_food, shedule, shedule_length, meat_flag)
 
-    def linear_randomize(self, count_of_dishes: int, used_food: list[Dish], list_food: list[Dish]):
-        unused_food = list_food
-        if used_food != []: unused_food = [elem for elem in list_food if elem not in used_food]
+    def _count_eating_time(self, list_food: list[Dish], remaining_time: int) -> list[Dish]:
+        """
+        :param list_food: список блюд, которые можно использовать
+        :param remaining_time: время, которое нужно занять одним блюдом
+        :return: если возможно, список еды, поедание которой занимает время, которое нужно занять, иначе весь список блюд, переданный изначально
+        """
+        all_eating_times = list(set([elem.eating_time for elem in list_food]))
+        if remaining_time in all_eating_times:
+            suitable_food = []
+            for food in list_food:
+                if food.eating_time == remaining_time:
+                    suitable_food.append(food)
+            return suitable_food
+        else:
+            return list_food
+
+    def _randomize(self, period_eating_time: (int, timedelta), used_food: list[Dish], list_food: list[Dish]) -> List[Dish]:
+        """
+        :param period_eating_time: время, на которое нужно составить расписание
+        :param used_food: еда того же типа, которая уже была использована в расписании
+        :param list_food: список еды одного типа
+        :return: расписание по одному типу еды на переданное время
+        """
+        if used_food:
+            unused_food = []
+            for food in list_food:
+                if food not in used_food:
+                    unused_food.append(food)
+            list_food = unused_food
+
         shedule = []
-        count_of_dishes_in_shedule = 0
-        while count_of_dishes_in_shedule < count_of_dishes:
-            condidate = random.choice(unused_food)
-            if condidate.eating_time > count_of_dishes - count_of_dishes_in_shedule:
-                break_count = 0
-                while condidate.eating_time > count_of_dishes - count_of_dishes_in_shedule and break_count < 100:
-                    condidate = random.choice(unused_food)
-                    break_count += 1
+        summary_eating_time = 0
+        while summary_eating_time < period_eating_time:
+            remaining_time = period_eating_time - summary_eating_time
+            condidate = random.choice(self._count_eating_time(list_food, remaining_time))
             shedule.append(condidate)
-            unused_food.pop(unused_food.index(condidate))
-            count_of_dishes_in_shedule += condidate.eating_time
+            list_food.remove(condidate)
+            summary_eating_time += condidate.eating_time
         return shedule
 
-    def randomize(self, count_of_dishes: int, food_devices: list[FoodDevice], preference: Preference):
-        foods = self._get_candidates(preference, food_devices)
 
-        breakfasts = []
-        garnish = []
-        meat = []
-        fruits = []
-        others = []
+    def get_shedule(self, period_eating_time: int, food_devices: list[FoodDevice], preference: Preference) -> tuple[list[Dish], list[Dish], list[Dish], list[Dish]]:
+        """
+        :param period_eating_time: время, на которое нужно составить расписание
+        :param food_devices: девайсы, которые можно использовать в готовке
+        :param preference: сезонные предпочтения
+        :return: отдельные расписания по каждому типу еды
+        """
+        foods = self._get_candidates(preference, food_devices)
+        breakfasts, garnish, meat, fruits, desserts, snacks, salads = [], [], [], [], [], [], []
         for food in foods:
             if food.food_type == "завтрак":
                 breakfasts.append(food)
@@ -66,23 +81,18 @@ class WeekGenerator:
                 meat.append(food)
             elif food.food_type == "фрукт":
                 fruits.append(food)
-            else:
-                others.append(food)
+            elif food.food_type == "сладкое":
+                desserts.append(food)
+            elif food.food_type == "закуска":
+                snacks.append(food)
+            elif food.food_type == "салат":
+                salads.append(food)
 
-        # list_food_breakfasts = self.recursive_randomize(count_of_dishes, breakfasts, [], [], 0, False)
-        # list_food_garnish = self.recursive_randomize(count_of_dishes, garnish, [], [], 0, False)
-        # list_food_meat = []
-        # for elem in list_food_garnish:
-        #     list_food_meat += self.recursive_randomize(elem.eating_time, meat, list_food_meat, [], 0, True)
-        # list_food_others = self.recursive_randomize(count_of_dishes // 2, others, [], [], 0, False) + [random.choice(fruits)]
-
-        list_food_breakfasts = self.linear_randomize(count_of_dishes, [], breakfasts)
-        list_food_garnish = self.linear_randomize(count_of_dishes, [], garnish)
+        list_food_breakfasts = self._randomize(period_eating_time, [], breakfasts)
+        list_food_garnish = self._randomize(period_eating_time, [], garnish)
         list_food_meat = []
-        for elem in list_food_garnish:
-            list_food_meat += self.linear_randomize(elem.eating_time, list_food_meat, meat)
-        list_food_others = self.linear_randomize(count_of_dishes, [], others) + [random.choice(fruits)]
+        for garnish in list_food_garnish:
+            list_food_meat += self._randomize(garnish.eating_time, list_food_meat, meat)
+        list_food_others = [random.choice(fruits), random.choice(salads), random.choice(snacks), random.choice(desserts)]
 
-        return (list_food_breakfasts, list_food_garnish, list_food_meat, list_food_others)
-# Создаёт расписание
-# Подходящие блюда задаются либо способом приготовления, либо типом еды
+        return list_food_breakfasts, list_food_garnish, list_food_meat, list_food_others
